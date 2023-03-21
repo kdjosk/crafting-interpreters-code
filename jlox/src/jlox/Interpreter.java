4e83d8c2339b21cbdd99eb7628bd944744d2ee76
Krzysztof Jóskowiak <krzysztof.josk@gmail.com>
2023-03-21 22:09:33 +0100

    Add closures

--- jlox/src/jlox/Interpreter.java
+++ jlox/src/jlox/Interpreter.java
@@ -104,7 +104,7 @@ class Interpreter implements Expr.Visitor<Object>,
   
   @Override
   public Void visitFunctionStmt(Function stmt) {
-    LoxFunction function = new LoxFunction(stmt);
+    LoxFunction function = new LoxFunction(stmt, environment);
     environment.define(stmt.name.lexeme, function);
     return null;
   }
