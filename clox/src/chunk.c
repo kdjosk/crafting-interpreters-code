f07038b300a2fd8e33fc7e6dfadd0950790c4ad9
Krzysztof Jóskowiak <krzysztof.josk@gmail.com>
2023-03-29 00:33:14 +0200

    Add run-length encoding for line numbers

--- clox/src/chunk.c
+++ clox/src/chunk.c
@@ -5,7 +5,9 @@
 
 void initChunk(Chunk* chunk) {
     chunk->count = 0;
+    chunk->countLines = 0;
     chunk->capacity = 0;
+    chunk->capacityLines = 0;
     chunk->code = NULL;
     chunk->lines = NULL;
     initValueArray(&chunk->constants);
@@ -17,18 +19,54 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code,
             oldCapacity, chunk->capacity);
-        chunk->lines = GROW_ARRAY(int, chunk->lines, 
-            oldCapacity, chunk->capacity);
+
     }
 
+    if (chunk->capacityLines < chunk->countLines + 2) {
+        int oldCapacity = chunk->capacityLines;
+        chunk->capacityLines = GROW_CAPACITY(oldCapacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, 
+            oldCapacity, chunk->capacityLines);
+    } 
+
     chunk->code[chunk->count] = byte;
-    chunk->lines[chunk->count] = line;
     chunk->count++;
+
+    /* For storing lines I used a run-length encoding
+        n_bytes, line, n_bytes, line, .., e.g
+        4, 1, 3, 2, ..
+        for 4 bytes on line 1, 3 bytes on line 2, etc
+    */
+    if (chunk->countLines >= 2 && chunk->lines[chunk->countLines - 1] == line) {
+        chunk->lines[chunk->countLines - 2]++;
+    } else {
+        chunk->lines[chunk->countLines] = 1;
+        chunk->lines[chunk->countLines + 1] = line;
+        chunk->countLines += 2;
+    }
+}
+
+int getLine(Chunk *chunk, int offset) {
+    /* For storing lines I used a run-length encoding
+        n_bytes, line, n_bytes, line, .., e.g
+        4, 1, 3, 2, ..
+        for 4 bytes on line 1, 3 bytes on line 2, etc
+    */
+    for (int i = 0; i < chunk->countLines; i += 2) {
+        if (offset <= chunk->lines[i]) {
+            return chunk->lines[i + 1];
+        } else {
+            offset -= chunk->lines[i];
+        }
+    }
+
+    // this should never happen, but if it does it means there's a bug
+    exit(1);
 }
 
 void freeChunk(Chunk *chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    FREE_ARRAY(int, chunk->lines, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacityLines);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
--- clox/src/chunk.h
+++ clox/src/chunk.h
@@ -12,6 +12,8 @@ typedef enum {
 typedef struct {
     int count;
     int capacity;
+    int countLines;
+    int capacityLines;
     uint8_t* code;
     int* lines;
     ValueArray constants;
@@ -21,5 +23,6 @@ void initChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 void freeChunk(Chunk* chunk);
 int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int offset);
 
 #endif
